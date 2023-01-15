8763509ef595f0ed6d2ee16b58db4c85efa90702
mls8504 <112783182+schierm@users.noreply.github.com>
2023-01-15 16:26:57 -0500

    main menu addition and added TMP package

---
+++ GlobalGameJameGame/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
